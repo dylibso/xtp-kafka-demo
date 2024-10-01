56df2d9ba5a9bc3a673629869a8e05549429d891
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-10-01 14:16:05 +0200

    kafka-pdk
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

---
+++ kafka-pdk/go.mod
@@ -0,0 +1,5 @@
+module kafka-pdk
+
+go 1.22.1
+
+require github.com/extism/go-pdk v1.0.5
---
+++ kafka-pdk/main.go
@@ -0,0 +1,10 @@
+// Note: run `go doc -all` in this package to see all of the types and functions available.
+// ./pdk.gen.go contains the domain types from the host where your plugin will run.
+package main
+
+// This function takes a Record and returns a Record.
+// It takes Record as input (A plain key/value record.)
+// And returns Record (A plain key/value record.)
+func Transform(input Record) (Record, error) {
+	return input, nil
+}
