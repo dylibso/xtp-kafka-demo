1830eb1f8dcea8fde71065f39ace85c1f025508e
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-10-02 18:51:55 +0200

    wip
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- quarkus-xtp-kafka-demo/src/main/java/com/dylibso/examples/kafka/simulation/Simulator.java
+++ quarkus-xtp-kafka-demo/src/main/java/com/dylibso/examples/kafka/simulation/Simulator.java
@@ -1,14 +1,5 @@
 package com.dylibso.examples.kafka.simulation;
 
-import io.quarkus.runtime.Startup;
-import io.quarkus.scheduler.Scheduled;
-import io.smallrye.reactive.messaging.kafka.KafkaRecord;
-import jakarta.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.eclipse.microprofile.reactive.messaging.Message;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
 import java.io.BufferedReader;
@@ -16,33 +7,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@ApplicationScoped
 public class Simulator {
     private static final Logger LOGGER = Logger.getLogger(Simulator.class);
 
-    @ConfigProperty(name = "simulator.enabled", defaultValue = "true")
-    boolean enabled;
-
-    @Channel("price-simulator")
-    Emitter<byte[]> emitter;
-
     private BufferedReader reader;
 
-    Simulator() throws IOException {
-        this.reader = new BufferedReader(new FileReader("../EURUSD_1min.txt"));
+    Simulator(String fileName) throws IOException {
+        this.reader = new BufferedReader(new FileReader(fileName));
         String heading = reader.readLine();// skip header.
         LOGGER.infof("heading: %s", heading);
     }
 
-    @Scheduled(every = "1s")
-    void produce() throws IOException {
+    byte[] next() throws IOException {
         var line = reader.readLine();
         if (line == null) {
             LOGGER.info("EOF");
-            return;
+            return null;
         }
         LOGGER.info(line);
-        emitter.send(KafkaRecord.of("EURUSD".getBytes(StandardCharsets.UTF_8), line.getBytes(StandardCharsets.UTF_8)));
+        return line.getBytes(StandardCharsets.UTF_8);
     }
+
 
 }
